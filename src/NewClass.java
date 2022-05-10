190e88e3742cb76da0cf3bf2d2dbb6bd92c2aa06
Fhamidullah <Fhamidullah>
2022-05-10 16:51:14 -0700

    change has been created a-b with C

--- src/NewClass.java
+++ src/NewClass.java
@@ -4,6 +4,15 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/**
+		 * This 
+		 * 
+		 */
+		
+		int a = 10;
+		int b = 10;
+		int c = a-b;
+		System.out.println(c);
 	}
 
 }
